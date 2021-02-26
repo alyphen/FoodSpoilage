f6a62ffa363c10caa9f6e6e25fde9cb571296ee6
dmccoystephenson <dmccoystephenson@gmail.com>
2021-02-25 21:38:51 -0700

    added try/catch blocks to each of the attempts to register a event handler class

--- src/main/java/spoilagesystem/EventRegistry.java
+++ src/main/java/spoilagesystem/EventRegistry.java
@@ -30,12 +30,45 @@ public class EventRegistry {
         FoodSpoilage mainInstance = FoodSpoilage.getInstance();
         PluginManager manager = mainInstance.getServer().getPluginManager();
 
-        manager.registerEvents(new CraftItemEventHandler(), mainInstance);
-        manager.registerEvents(new InventoryDragEventHandler(), mainInstance);
-        manager.registerEvents(new PlayerInteractEventHandler(), mainInstance);
-        manager.registerEvents(new ItemSpawnEventHandler(), mainInstance);
-        manager.registerEvents(new FurnaceSmeltEventHandler(), mainInstance);
-        manager.registerEvents(new BlockCookEventHandler(), mainInstance);
+        try {
+            manager.registerEvents(new CraftItemEventHandler(), mainInstance);
+        } catch(Exception e) {
+            System.out.println("CraftItemEventHandler was not able to be registered.");
+        }
+
+        try {
+            manager.registerEvents(new InventoryDragEventHandler(), mainInstance);
+        } catch(Exception e) {
+            System.out.println("InventoryDragEventHandler was not able to be registered.");
+        }
+
+        try {
+            manager.registerEvents(new PlayerInteractEventHandler(), mainInstance);
+        } catch(Exception e) {
+            System.out.println("PlayerInteractEventHandler was not able to be registered.");
+        }
+
+        try {
+            manager.registerEvents(new ItemSpawnEventHandler(), mainInstance);
+        } catch(Exception e) {
+            System.out.println("ItemSpawnEventHandler was not able to be registered.");
+        }
+
+        try {
+            manager.registerEvents(new FurnaceSmeltEventHandler(), mainInstance);
+        } catch(Exception e) {
+            System.out.println("FurnaceSmeltEventHandler was not able to be registered.");
+        }
+
+        try {
+            manager.registerEvents(new BlockCookEventHandler(), mainInstance);
+        } catch(Exception e) {
+            System.out.println("BlockCookEventHandler was not able to be registered.");
+        }
+
+
+
+
 
     }
 
